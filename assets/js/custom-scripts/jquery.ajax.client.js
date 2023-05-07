4efc0862c87f13dd73ed810572aadabebe753cc3
Bhaskar Bhakt <bhaskarbhakt10@gmail.com>
2023-05-07 13:37:32 +0530

    check if client already exists

--- assets/js/custom-scripts/jquery.ajax.client.js
+++ assets/js/custom-scripts/jquery.ajax.client.js
@@ -7,9 +7,18 @@
         e.preventDefault();
         let this_form = $(this).closest('form');
         let form_data = this_form.serializeArray();
+        let client_name = $(this_form).find('[name="client-name"]').val();
+        let client_country_code = $(this_form).find('[name="address-country-code"]').val();
+        let client_phone_number = $(this_form).find('[name="client-phone-number"]').val();
+        let client_email = $(this_form).find('[name="client-email"]').val();
+        console.log(client_name, client_country_code, client_phone_number, client_email);
         let data = {
             "form_data": form_data,
-        }
+            "client_name" : client_name,
+            "client_country_code" : client_country_code,
+            "client_phone_number" : client_phone_number,
+            "client_email":client_email
+        }   
         $.ajax({
             url: ajax_url,
             type: 'POST',
@@ -20,8 +29,8 @@
                     if (response[key] === true) {
                         let success_html = '<div class="alert alert-success alert-dismissible fade show mt-5" role="alert"> ';
                         success_html += '<strong>Client added Sucessfully!</strong>  ';
-                        success_html += '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> '
-                        success_html += '</div>'
+                        success_html += '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> ';
+                        success_html += '</div>';
                         $(this_form)[0].reset();
                         $(this_form).prepend(success_html);
                         $('[name=textbox-usage]').trigger('change');
@@ -29,16 +38,26 @@
                             $('form .alert').remove();
                         }, 3000);
                     }
-
+                    if (response[key] === false) {
+                        let exists_html = '<div class="alert alert-warning alert-dismissible fade show mt-5" role="alert"> ';
+                        exists_html += '<strong>Client Already Exists!</strong>  ';
+                        exists_html += '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> ';
+                        exists_html += '</div>';
+                        $(this_form)[0].reset();
+                        $(this_form).prepend(exists_html);
+                        $('[name=textbox-usage]').trigger('change');
+                        setTimeout(() => {
+                            $('form .alert').remove();
+                        }, 3000);
+                    }
                 }
             },
             error: function (error) {
                 window.alert(error);
             }
         });
-
-
     });
+
 
 
 
--- classes/actions/add-client.php
+++ classes/actions/add-client.php
@@ -4,6 +4,13 @@ require_once ROOT_PATH_CLASS . 'client/class.client.php';
 //ajax-file for client adding
 if (isset($_POST)) {
     $post_data = $_POST['form_data'];
+    $client_name = $_POST['client_name'];
+    $client_country_code = $_POST['client_country_code'];
+    $client_phone_number = $_POST['client_phone_number'];
+    $client_email = $_POST['client_email'];
+
+
+
     $client_arr = array();
     foreach ($post_data as $post) {
         $client_arr[$post['name']] = $post['value'];
@@ -11,11 +18,29 @@ if (isset($_POST)) {
     // print_r($client_arr);
 
     $client = new Client();
+    $results = $client->get_all_clients();
+    $all_clients = array();
+    if($results->num_rows>0){
+        while($row = $results->fetch_assoc()){
+            $row__ = json_decode($row['InvoiceClient_Info'], true);
+            array_push($all_clients, $row__);
+        }
+    }
+    
     $response_array = array();
-    if ($client->get_details($client_arr) === true) {
-        $response_array['response'] = true;
-    } else {
-        $response_array['response'] = false;
+    foreach($all_clients as $all){
+        if(strcasecmp($all['client-name'] , $client_name) === 0 && strcasecmp($all['address-country-code'], $client_country_code ) === 0 && strcasecmp($all['client-phone-number'], $client_phone_number)===0 && strcasecmp($all['client-email'], $client_email) === 0){
+            // echo "here228882";
+            $response_array['response'] = false;
+        }
+        else{
+            if ($client->get_details($client_arr) === true) {
+                $response_array['response'] = true;
+            } else {
+                $response_array['response'] = false;
+            }
+
+        }
     }
     echo json_encode($response_array);
 }
