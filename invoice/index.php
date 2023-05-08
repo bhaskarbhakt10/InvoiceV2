42f8eaed8833e7d7f43ce0670be7ed648d4e269e
dev2698 <dev@scaleup42.com>
2023-05-08 20:47:57 +0530

    final cNAGES

--- invoice/index.php
+++ invoice/index.php
@@ -3,29 +3,43 @@ require_once '../config.php';
 require_once 'parts/header.php';
 
 $all_dir = scandir(ROOT_PATH . 'invoice/');
-
-if (array_key_exists('page', $_GET)) {
-    $page = $_GET['page'];
 ?>
-    <main class="<?php echo $page; ?>">
-        <div class="container-fluid">
+<main >
+    <div class="container-fluid">
         <?php
-        if (in_array($page  . ".php", $all_dir)) {
-            foreach ($all_dir as $d) {
-                if ($d === $page  . ".php") {
-                    require_once ROOT_PATH . 'invoice/' . $page  . ".php";
+        if (array_key_exists('page', $_GET)) {
+        ?>
+            <div class="<?php echo $page; ?>">
+                <?php
+                $page = $_GET['page'];
+                if (in_array($page  . ".php", $all_dir)) {
+                    foreach ($all_dir as $d) {
+                        if ($d === $page  . ".php") {
+                            require_once ROOT_PATH . 'invoice/' . $page  . ".php";
+                        }
+                    }
+                } else {
+                    echo "no file";
                 }
-            }
-        } else {
-            echo "no file";
+                ?>
+            </div>
+        <?php
         }
         ?>
+        <!-- //index page start  -->
+        <div class="index-wrapper">
+            <div class="row">
+                <div class="col-md-12">
+                    <div>
+                        <button class="btn"></button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </main>
+        <!-- //index page end  -->
+    </div>
+</main>
+
+
 <?php
-}
-
-
-
-
 require_once 'parts/footer.php';
