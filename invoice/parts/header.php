f4dd88cad9d74b4d65c9459ed1a11f656cc7c784
ujwal joshi <ujwal@nuitsolutions.com>
2023-05-18 22:04:57 +0530

    Header Added

--- invoice/parts/header.php
+++ invoice/parts/header.php
@@ -42,5 +42,32 @@
 
     ?>
 </head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+          <a class="navbar-brand" href="#">NUIT Invoice</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo NAVIGATION_URL .'invoice-edit-clients' ?>">Add Client</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo NAVIGATION_URL .'invoice-client-list' ?>">List Client</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo NAVIGATION_URL .'invoice-list-proforma' ?>">List Proforma</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo NAVIGATION_URL .'invoice-clients' ?>">List Invoices</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+</header>
 
 <body>
