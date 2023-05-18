a1bf2585f5d64bd6a4b932431d68ff14c2369f27
ujwal joshi <ujwal@nuitsolutions.com>
2023-05-18 22:13:26 +0530

    Header logo & favicon added

--- invoice/parts/header.php
+++ invoice/parts/header.php
@@ -11,6 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Invoice </title>
+    <link rel="shortcut icon" href="http://www.nuitsolutions.com/wp-content/uploads/2016/03/favicon-32x32.png">
     <!-- //dependency  -->
     <?php
     $styles_dep = scandir(ROOT_PATH . 'assets/css/dependency-css');
@@ -43,9 +44,9 @@
     ?>
 </head>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <div class="container">
-          <a class="navbar-brand" href="#">NUIT Invoice</a>
+          <a class="navbar-brand" href="/"><img src="https://www.nuitsolutions.com/wp-content/uploads/2022/03/NUIT-Logo-2.svg" alt="logo" width="200"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -53,16 +54,16 @@
           <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
-                <a class="nav-link" href="<?php echo NAVIGATION_URL .'invoice-edit-clients' ?>">Add Client</a>
+                <a class="nav-link text-white" href="<?php echo NAVIGATION_URL .'invoice-edit-clients' ?>">Add Client</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="<?php echo NAVIGATION_URL .'invoice-client-list' ?>">List Client</a>
+                <a class="nav-link text-white" href="<?php echo NAVIGATION_URL .'invoice-client-list' ?>">List Client</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">List Proforma</a>
+                <a class="nav-link text-white" href="#">List Proforma</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">List Invoices</a>
+                <a class="nav-link text-white" href="#">List Invoices</a>
               </li>
             </ul>
           </div>
