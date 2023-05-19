3b2e47f6af57adc228a0ee3a59dc8e65e5d38d1d
ujwal joshi <ujwal@nuitsolutions.com>
2023-05-19 21:36:08 +0530

    Terms & Condition added and Header logo url updated

--- invoice/parts/invoice-header.php
+++ invoice/parts/invoice-header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/"><img src="<?php echo LOGO; ?>" alt="logo" width="200"></a>
+            <a class="navbar-brand" href="#"><img src="<?php echo LOGO; ?>" alt="logo" width="200"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
