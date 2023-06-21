3ef612e421d6265f355a0a472fcd0a809dbe156e
ujwal joshi <ujwal@nuitsolutions.com>
2023-06-21 19:50:31 +0530

    Added a simple confirmation box for delete btn in client list

--- assets/js/custom-scripts/jquery.ajax.sodtdelclient.js
+++ assets/js/custom-scripts/jquery.ajax.sodtdelclient.js
@@ -1,30 +1,42 @@
-(function($){
-    let ajax_url = $('#ajax_url').val();
-    // console.log(ajax_url);
-    $('#ajax_url').remove();
-    $(document.body).on('click', '.delete', function(){
-        let this_btn = $(this);
-        let closest_row = $(this).closest('tr');
-        let delete_id = $(this).attr('data-delete');
-        let data ={
-            'delete_id': delete_id
-        };
-        $.ajax({
-            url:ajax_url,
-            type: 'POST',
-            data: data,
-            success: function (data) {
-                let response = JSON.parse(data);
-                for (const key in response) {
-                    if(response[key] === true){
-                        $(closest_row).remove();
-                    }
-                }
-            },
-            error: function (error) {
-                window.alert(error);
+(function ($) {
+  let ajax_url = $("#ajax_url").val();
+  // console.log(ajax_url);
+  $("#ajax_url").remove();
+  $(document.body).on("click", ".delete", function () {
+    let this_btn = $(this);
+    let closest_row = $(this).closest("tr");
+    let delete_id = $(this).attr("data-delete");
+    let data = {
+      delete_id: delete_id,
+    };
+    const reply = confirm_delete();
+    console.log(reply);
+    if (reply === true) {
+      $.ajax({
+        url: ajax_url,
+        type: "POST",
+        data: data,
+        success: function (data) {
+          let response = JSON.parse(data);
+          for (const key in response) {
+            if (response[key] === true) {
+              $(closest_row).remove();
             }
-        })
-    });
+          }
+        },
+        error: function (error) {
+          window.alert(error);
+        },
+      });
+    }
+  });
 
-})(jQuery)
+  function confirm_delete() {
+    let text = "Are you sure you want to Delete?";
+    if (confirm(text) === true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+})(jQuery);
